f13f77b8cb751ff77a1eb8f673563b12c093b36b
Wouter Landuydt <wouter.landuydt@gmail.com>
2017-11-26 16:38:17 +0100

    ✨ first commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,86 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    // BUG: flicker
+    ofSetBackgroundAuto(false);
+    
+    // Load image and set anchorpoint
+    testPlant.load("images/testplant.png");
+    testPlant.setAnchorPoint(testPlant.getWidth()/2, testPlant.getHeight()/2);
+    
+    // set delay after click
+    delay = 3000;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    // Log clicked x and y position.
+    ofLog() << "xpos: " << x << " ypos: " << y;
+    
+    // Set delay before drawing plant
+    ofSleepMillis(delay);
+    
+    // draws plant at pressed position
+    testPlant.draw(x, y);
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
+}
