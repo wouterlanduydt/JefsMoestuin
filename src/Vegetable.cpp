5ebbe54cbd42a17d944aa894f2e1a0a53b3e2c3b
RuubenL <ruuben.l@live.be>
2018-01-05 17:32:44 +0100

    🐛 solved pause bug and used scale instead of fade

--- src/Vegetable.cpp
+++ src/Vegetable.cpp
@@ -5,14 +5,22 @@ void Vegetable::draw() {
 }
 
 void Vegetable::update() {
-    video.update();
+
     if (video.getCurrentFrame() == 115) {
-        pausedTime = ofGetElapsedTimeMillis() + (60000 * ofRandom(5, 10));
+        video.update();
+        pausedTime = ofGetElapsedTimeMillis() + (5000);
     }
 
-    if (ofGetElapsedTimeMillis() <= pausedTime) {
-        video.setFrame(115);
+    if (ofGetElapsedTimeMillis() >= pausedTime && scale >= 0) {
+        video.update();
+        if (video.getCurrentFrame() == 116) {
+            scale-=.01;
+        }
+        ofLog() << "playing at " << video.getCurrentFrame();
+    } else if (scale <= 0) {
+        video.close();
     }
+    
     video.setAnchorPoint((video.getWidth() * scale) / 2, (video.getHeight() * scale) / 2);
     video.setSpeed(growSpeed);
     video.setLoopState(OF_LOOP_NONE);
